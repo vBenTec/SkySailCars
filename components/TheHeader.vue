8904647bf23f4f3afc333f347eab4a70dc4e184e
Ben <bentec.dev@gmail.com>
2023-12-21 16:30:24 +0000

    make top header responsive

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -10,15 +10,31 @@ const handleSearch = (value) => {
 </script>
 
 <template>
-  <header>
-    <base-logo/>
-    <base-search @handle:search="handleSearch" placeholder="Search something here"/>
-    <base-icon tag="button" styling="circleNeutral" icon="heart-outline"/>
+  <header class="header">
+    <div class="header__logo">
+      <base-logo/>
+    </div>
+    <base-search class="header__search" @handle:search="handleSearch" placeholder="Search something here"/>
+    <div class="header__btn">
+      <base-icon tag="button" styling="circleNeutral" icon="heart-outline"/>
+    </div>
   </header>
 </template>
 
 <style scoped lang="postcss">
-header {
-  @apply flex items-center justify-between bg-white border-default panel-spacing;
+.header {
+  @apply grid grid-cols-2 gap-2 md:gap-0 md:flex md:items-center md:justify-between bg-white md:border-default panel-spacing;
+
+  &__logo {
+    @apply row-start-1 row-end-2 col-start-1 col-end-2 md:basis-[20%];
+  }
+
+  &__search {
+    @apply row-start-2 col-span-2 md:flex-grow md:max-w-2xl;
+  }
+
+  &__btn {
+    @apply row-start-1 row-end-2 col-start-2 col-end-3 md:basis-[20%] flex justify-end;
+  }
 }
 </style>
